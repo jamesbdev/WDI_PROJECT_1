c604c2d04629bb6796dcda95f8238547e3e613dc
jaybaybremner <j.bremner@ymail.com>
2016-12-13 10:15:11 +0000

    working game

--- js/app.js
+++ js/app.js
@@ -1,48 +1,35 @@
-console.log('loaded');
-//click event listeners
-//make sounds play after click
-//match sounds
-//if sounds are equal, remove them
-//shuffle board
 
-
-//DOM
-//select elements sounds by class name
-//add click element individually
-
-//Logic
-//give value to individual elements
-//after clicking 2 elements, check value
-//if values are equal to each other, make elements 'sleep'
-//if not keep as is (or shuffle board)
-//if all sounds found, notify user of win
-
-//extra: scoring display
-//difficulty level
-
-//play background music
-//make squares light up when match
-//disable self-match
-//score-board: 1 match - 9 to go
+//matches-board: 1 match - 9 to go
 //after all matches, play win song
-var score = 0;
-var counter = 0;
+
+var matches;
+var counter;
 var firstPick = null;
 var secondPick = null;
+var backgroundMusic;
+var winMusic;
 var sounds = ['he-he-he', 'bassline', 'gangnam_style', 'go', 'heey', 'noise', 'ooh', 'oppan', 'synth', 'ukwis', 'he-he-he', 'bassline', 'gangnam_style', 'go', 'heey', 'noise', 'ooh', 'oppan', 'synth', 'ukwis'];
 
+
 function startMusic() {
-  var backgroundMusic = new Audio('sounds/sound_loop.wav');
+  backgroundMusic = new Audio('sounds/sound_loop.wav');
   backgroundMusic.loop = true;
   backgroundMusic.play();
 }
 
 $(function() {
   startMusic();
-  $('#score').text(score);
+  matches = 0;
+  counter = 0;
+  firstPick = null;
+  secondPick = null;
+  $('#matchesLeft').text(10 - matches);
+  var sounds = ['he-he-he', 'bassline', 'gangnam_style', 'go', 'heey', 'noise', 'ooh', 'oppan', 'synth', 'ukwis', 'he-he-he', 'bassline', 'gangnam_style', 'go', 'heey', 'noise', 'ooh', 'oppan', 'synth', 'ukwis'];
   $.each(sounds, createBoard);
   $('li').click(setChoices);
 });
+
+
 
 function createBoard(index) {
   var random = Math.floor(Math.random() * sounds.length);
@@ -52,9 +39,17 @@ function createBoard(index) {
   sounds.splice(random, 1);
 }
 
-function checkBoard() {
+function checkMatches() {
   //if class won all checked, call win
-
+  if (matches === 10) {
+    backgroundMusic.pause();
+    winMusic = new Audio('sounds/win_song.wav');
+    winMusic.play();
+    $(winMusic).on('ended', function() {
+      backgroundMusic.play();
+      // resetGame();
+    });
+  }
 }
 
 function setChoices(){
@@ -65,14 +60,8 @@ function setChoices(){
   counter++;
 
   if (counter % 2 !== 0) {
-    // first click
     firstPick = $(this);
   } else {
-    // second click
-    // at the second click, when we have both values to compare
-    // e.g firstPick was 1 and secondPick was 2
-    // if firstPick is the same as secondPick, it's a match
-    // if firstPick is not the same as secondPick, it's not a match
     secondPick = $(this);
     console.log('first', firstPick.attr('data-sound'), 'second', secondPick.attr('data-sound'));
 
@@ -82,13 +71,13 @@ function setChoices(){
         console.log('Match');
         firstPick.addClass('won');
         secondPick.addClass('won');
-        score = score + 5;
-        checkBoard();
+        matches = matches + 1;
+        checkMatches();
       } else {
         console.log('not a match');
-        score = score - 1;
+
       }
-      $('#score').text(score);
+      $('#matchesLeft').text(10 - matches);
     } else {
       console.log('it\'s the same, cheater');
     }
